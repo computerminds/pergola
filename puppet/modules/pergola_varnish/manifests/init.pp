3a3a83a2a9f2f2adcc35d91abe952105ee6bf9d2
Steven Jones <steven.jones@computerminds.co.uk>
2012-01-23 17:44:04 +0000

    Remove some un-needed stuff.

--- puppet/modules/pergola_varnish/manifests/init.pp
+++ puppet/modules/pergola_varnish/manifests/init.pp
@@ -13,13 +13,6 @@ define pergola_varnish($storage="malloc",
   if ($ensure == "running") {
     varnish::instance { $instance:
       #backend      => "localhost:8080",
-      #address      => ["192.168.1.10:80"],
-      #admin_port   => "6082",
-      #storage        => ["file,/var/varnish/storage1.bin,90%",
-      #                   "file,/var/varnish/storage2.bin,90%"],
-      #params       => ["thread_pool_min=1",
-      #                 "thread_pool_max=1000",
-      #                 "thread_pool_timeout=120"],
       vcl_file   => "puppet:///modules/pergola_varnish/drupal.vcl"
     }
   }
