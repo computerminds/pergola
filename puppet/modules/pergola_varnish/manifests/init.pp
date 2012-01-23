a11b6ff4cb8dc6308c55ba2b31328e02b8e4fd19
Steven Jones <steven.jones@computerminds.co.uk>
2012-01-23 17:00:28 +0000

    Add a basic varnish config

---
+++ puppet/modules/pergola_varnish/manifests/init.pp
@@ -0,0 +1,24 @@
+class pergola_varnish($storage="malloc",
+                      $ensure="running",
+                      $vcl="UNSET") {
+  
+  $instance = $name
+  
+  if ! ($ensure in [ "running", "stopped" ]) {
+    fail("ensure parameter must be running or stopped")
+  }
+  
+  if ($ensure == "running") {
+    varnish::instance { instance
+      #backend      => "localhost:8080",
+      #address      => ["192.168.1.10:80"],
+      #admin_port   => "6082",
+      #storage        => ["file,/var/varnish/storage1.bin,90%",
+      #                   "file,/var/varnish/storage2.bin,90%"],
+      #params       => ["thread_pool_min=1",
+      #                 "thread_pool_max=1000",
+      #                 "thread_pool_timeout=120"],
+    }
+  }
+
+}
--- puppet/pergola.pp
+++ puppet/pergola.pp
@@ -1,10 +1,19 @@
 # Default puppet configuration for pergola.
 # Try not to edit this file!
 node default {
+  
+  # Ensure ntp is installed.
   class { ntp:
     ensure     => running,
     autoupdate => true,
   }
   
+  # Ensure the auto updater is around
   include pergola_updater
+  
+  # Ensure Varnish is installed
+  pergola_varnish { 'default',
+    
+  }
+  
 }
