3f848bc56dafb80b5a8f34a20a8a8c58108c92c4
Steven Jones <steven.jones@computerminds.co.uk>
2012-01-30 10:02:29 +0000

    Don't use colour when calling the puppet script.

--- fab/puppet.py
+++ fab/puppet.py
@@ -6,4 +6,4 @@ from fabric.api import *
 
 def run_puppet():
     """run puppet to get the config applied."""
-    local('puppet apply --verbose --modulepath=/opt/pergola/puppet/modules /opt/pergola/puppet/pergola.pp', capture=False)
+    local('puppet apply --color=false --verbose --modulepath=/opt/pergola/puppet/modules /opt/pergola/puppet/pergola.pp', capture=False)
