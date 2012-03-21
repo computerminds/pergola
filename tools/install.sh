1165479b3f78b81825c0d06da4ddffa3e6a4985f
Steven Jones <steven.jones@computerminds.co.uk>
2012-03-21 11:41:47 +0000

    Force the install of git.

--- tools/install.sh
+++ tools/install.sh
@@ -7,7 +7,7 @@ fi
 echo "Installing git"
 
 /usr/bin/env apt-get update
-/usr/bin/env apt-get install git-core
+/usr/bin/env apt-get install --yes git-core
 
 echo "Installing pergola"
 /usr/bin/env git clone git://github.com/computerminds/pergola.git -b dev /opt/pergola
