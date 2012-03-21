6f68d277b184df66094ebb4930014f5e7fa6298c
Steven Jones <steven.jones@computerminds.co.uk>
2012-03-21 11:16:55 +0000

    Add a simple installer

---
+++ tools/install.sh
@@ -0,0 +1,16 @@
+if [ -d /opt/pergola ]
+then
+  echo "You have already installed Pergola"
+  exit
+fi
+
+echo "Installing git"
+
+/usr/bin/env apt-get update
+/usr/bin/env apt-get install git-core
+
+echo "Installing pergola"
+/usr/bin/env git clone git://github.com/computerminds/pergola.git -b dev /opt/pergola
+
+echo "Setting up Pergola"
+/usr/bin/env cd /opt/pergola && python setup.py
